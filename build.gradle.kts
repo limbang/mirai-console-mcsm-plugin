58584cf2463f50d80e1bf6a658f2d1b95bb1fa11
limbang <495071565@qq.com>
2022-06-19 16:12:03 +0800

    build: 更新依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.7.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "top.limbang.mcsm"
-version = "1.0.5"
+version = "1.0.6"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
@@ -19,9 +19,10 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("top.limbang:mirai-plugin-general-interface:1.0.0")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.1")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.6.1")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.6.2")
     testImplementation( "com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
 }
