bf77abee327fcf9887e59026bcbec46f07c2e312
limbang <495071565@qq.com>
2026-01-20 18:08:05 +0800

    chore(build): 更新项目依赖版本
    - 将 Kotlin 版本从 1.9.0 升级到 2.3.0
    - 将项目版本从 1.1.6 更新为 1.2.0
    - 更新 Gradle 包装器版本从 7.1 到 7.6.3
    - 升级 Retrofit 库从 2.9.0 到 3.0.0
    - 升级 OkHttp 库从 5.0.0-alpha.11 到 5.3.2
    - 升级 KotlinX Serialization 从 1.5.0 到 1.9.0
    - 升级 Kotlin Coroutines 测试库从 1.6.4 到 1.10.2
    - 添加 SLF4J 日志 API 依赖
    - 添加 Logback 经典实现用于测试环境
    - 移除 Okio 直接依赖并升级日志拦截器版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "1.9.0"
+    val kotlinVersion = "2.3.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
 
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "top.limbang"
-version = "1.1.6"
+version = "1.2.0"
 
 
 repositories {
@@ -17,14 +17,15 @@ repositories {
 
 dependencies {
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okio:okio:3.7.0")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.okhttp3:okhttp:5.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     compileOnly("top.limbang:mirai-plugin-general-interface:1.0.2")
 
     testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.6.4")
-    testImplementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    testImplementation("ch.qos.logback:logback-classic:1.5.25")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-debug:1.10.2")
+    testImplementation("com.squareup.okhttp3:logging-interceptor:5.3.2")
 }
