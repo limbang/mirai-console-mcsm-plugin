1c33df088e973e3afdd0bcc6f91ac1942fb86670
limbang <495071565@qq.com>
2026-05-30 18:32:25 +0800

    chore(version): 更新版本号从 1.2.1 到 1.2.2
    - 更新 build.gradle.kts 中的项目版本号
    - 更新 MCSM.kt 中的插件版本号

--- src/main/kotlin/MCSM.kt
+++ src/main/kotlin/MCSM.kt
@@ -33,7 +33,7 @@ object MCSM : KotlinPlugin(
     JvmPluginDescription(
         id = "top.limbang.mcsm",
         name = "MCSManager API",
-        version = "1.2.1",
+        version = "1.2.2",
     ) {
         author("limbang")
         info("MCSManager api 插件")
